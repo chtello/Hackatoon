472d87ecacacd85b2cad6d0020f827816a6c19cd
chtello <chtello@escuelasproa.edu.ar>
2024-09-03 14:19:28 -0300

    navbar casi listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
 import './App.css'
+import Nabvar from './componentes/Nabvar'
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [weatherData, setWeatherData] = useState(null);
-  const cityToSearch = "Cordoba";
+  const [datos, setDatos] = useState(null);
+  const [lugar,setLugar] = useState("");
   useEffect(() => {
     const fetchWeather = async () => {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=9c349c187d3a0e148101022324d4e5f8&units=metric`
+          `https://api.openweathermap.org/data/2.5/weather?q=${lugar}&appid=9c349c187d3a0e148101022324d4e5f8&units=metric`
         );
-        setWeatherData(response.data);
+        setDatos(response.data);
       } catch (error) {
         console.error('Error al obtener el clima:', error);
       }
     };
 
     fetchWeather();
-  }, [cityToSearch]);
+  }, [lugar]);
 
+  const busLug = (event) => {
+    setLugar(event.target.value)
+  }
   return (
     <>
     <div>
-      {weatherData ? (
+      <Nabvar/>
+      <input type="text" placeholder='ingrece su ciudad, pueblo, etc...' value={lugar} onChange={busLug}/>
+      <button onClick={() => setLugar(lugar)}>buscar</button>
+      {datos ? (
         <div>
-          <h3>Clima en {weatherData.name}</h3>
-          <p>Temperatura: {weatherData.main.temp}°C</p>
-          <p>Descripción: {weatherData.weather[0].description}</p>
+          <h3>Clima en {datos.name}</h3>
+          <p>Temperatura: {datos.main.temp}°C</p>
+          <p>Descripción: {datos.weather[0].description}</p>
         </div>
       ) : (
-        <p>Cargando...</p>
+        <p></p>
       )}
     </div>
     </>
---
+++ src/componentes/Nabvar.jsx
@@ -0,0 +1,17 @@
+import './Nabvar.css'
+function Nabvar() {
+  return (
+    <>
+      <div >
+      <nav class="navbar bg-body-tertiary fixed-top">
+  <div class="">
+      <img src="./logo.png"/>
+    <a class="navbar-brand" href="#">Weather Radar</a>
+  </div>
+</nav>
+      </div>
+    </>
+  )
+}
+
+export default Nabvar
